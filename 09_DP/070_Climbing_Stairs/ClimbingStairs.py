18a77b876567d18ab21430833a7fc68477832c63
dtran39 <trananhduc@gatech.edu>
2017-03-28 22:18:27 -0400

    Add description for climbing stairs and house robber

--- 09_DP/070_Climbing_Stairs/ClimbingStairs.py
+++ 09_DP/070_Climbing_Stairs/ClimbingStairs.py
@@ -1,3 +1,19 @@
+'''
+Problem:
+	- You are climbing a stair case. It takes n steps to reach to the top.
+
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+----------------------------------------------------------------------------------------------------
+Examples:
+
+----------------------------------------------------------------------------------------------------
+Solution:
+1. Recursion
+2. Top-down
+3. Bottom-up
+4. Math
+'''
 class Solution(object):
     def climbStairsFormula(self, n):
         from math import sqrt, pow
